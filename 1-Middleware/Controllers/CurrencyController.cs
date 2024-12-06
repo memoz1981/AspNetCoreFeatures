1987003b3af6255f56b7bc25b9f7da6d57ec4da6
M.Z. <zeynalov@gmail.com>
2024-12-06 15:19:46 +0400

    controller added to test route re-direction helper method.

---
+++ 1-Middleware/Controllers/CurrencyController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace _1_Middleware.Controllers
+{
+    public class CurrencyController : Controller
+    {
+        public IActionResult Index()
+        {
+            var url = Url.Action(nameof(View), "Currency", new { code = "USD" });
+            return Content($"The URL is {url}");
+        }
+
+        public new IActionResult View(string code)
+        {
+            return Ok(code); 
+        }
+    }
+}
