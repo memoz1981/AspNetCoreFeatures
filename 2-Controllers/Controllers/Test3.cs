4a0ec383f23a1c5977de273ffd1bc8599a75d6e4
M.Z. <zeynalov@gmail.com>
2024-12-06 15:38:24 +0400

    test added.

--- 2-Controllers/Controllers/Test3.cs
+++ 2-Controllers/Controllers/Test3.cs
@@ -4,6 +4,9 @@ namespace _2_Controllers.Controllers;
 
 public class Test3 : Test2Controller
 {
+    // if override (and virual in parent) not used - AmbigousMatchException is thrown
+    // attribute routing should return single match (rather than first match in conventional routing)
+    // method name can be different as well - in that case route name would probably need to be specified different from parent
     [HttpGet]
     public override IActionResult Get() => Ok("Test3 controller entered..."); 
 }
